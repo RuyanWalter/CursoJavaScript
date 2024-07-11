d19ebc97a681eb47d4483b5f7f1b3d06e405c78e
RuyanWalter <sirin23ruyan@gmail.com>
2024-07-11 16:13:42 -0600

    update

---
+++ Basic/01_variables.js
@@ -0,0 +1,27 @@
+//Vamos aprender a declarar Variables 
+
+//var
+var helloWorld = "¡Hello, JavasCript!"
+console.log(helloWorld)
+
+helloWorld =  "Hola de nuevo, JavaScript"
+console.log(helloWorld)
+
+//Estos son los mas usados para declarar variables 
+//let
+
+let helloWorld2 = "¡Hello, JavasCript 2!"
+console.log(helloWorld2)
+
+helloWorld2 = "Hola de nuevo, JavaScript 2"
+console.log(helloWorld2)
+
+//const
+//el ambito de ejecucion es igual a let pero no se puede reasignar puesto que es constante y no se puede modificar
+
+const helloWorld3 = "Hello, JavaScript 3"
+console.log(helloWorld3)
+
+//Al momento de ejecutar este codido dara error
+//helloWorld3 = "Hola de nuevo, JavaScript 3"
+//console.log(helloWorld3)
